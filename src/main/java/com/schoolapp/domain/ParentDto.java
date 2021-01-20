1a61a45b2f59d24a9ca3e3f4c0d6712c23f0559b
dud3k88 <mateusz.dudek88@gmail.com>
2021-01-20 16:21:33 +0100

    20.01_5

--- src/main/java/com/schoolapp/domain/ParentDto.java
+++ src/main/java/com/schoolapp/domain/ParentDto.java
@@ -2,9 +2,11 @@ package com.schoolapp.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class ParentDto {
